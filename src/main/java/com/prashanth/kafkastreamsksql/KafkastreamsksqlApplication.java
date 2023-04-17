d19bf99370462ef863045ae54add9047c397de82
Prashanth Sriram <prashanthsriram145@gmail.com>
2023-04-17 19:00:39 +0530

    Initial commit

---
+++ src/main/java/com/prashanth/kafkastreamsksql/KafkastreamsksqlApplication.java
@@ -0,0 +1,13 @@
+package com.prashanth.kafkastreamsksql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkastreamsksqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkastreamsksqlApplication.class, args);
+	}
+
+}
